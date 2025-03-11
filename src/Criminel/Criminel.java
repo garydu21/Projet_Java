1861df0e50dc7148bb5b344b2caf6f1372c428a6
garydu21 <angelnatpsg@gmail.com>
2025-03-11 17:16:39 +0100

    Development : Definition d'un Criminel et d'un Crime

--- src/Criminel/Criminel.java
+++ src/Criminel/Criminel.java
@@ -1,4 +1,37 @@
 package Criminel;
 
+import java.util.ArrayList;
+
 public class Criminel {
+
+    private String nom;
+    private String prenom;
+
+    private ArrayList<Crime> crimes;
+
+    private int peineTotale;
+
+    public Criminel(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.crimes = new ArrayList<Crime>();
+        this.peineTotale = 0;
+    }
+
+    public void ajouterCrime(Crime crime) {
+        this.crimes.add(crime);
+        this.peineTotale += crime.getPeine();
+    }
+
+    public int getPeineTotale() {
+        return this.peineTotale;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public String getPrenom() {
+        return this.prenom;
+    }
 }
