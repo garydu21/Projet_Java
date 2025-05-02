9255233c9f38021230076d02411db6d9f73e68b0
Plastik0 <chris.kalouche2005@gmail.com>
2025-05-02 13:14:08 +0200

    Ajout d'un systeme de graphe avec des liens entre les criminels et les différentes affaire, savoir si ils sont impliqué dans plusieurs affaire etc..

--- src/Vue/VueAffaires.java
+++ src/Vue/VueAffaires.java
@@ -13,7 +13,7 @@ public class VueAffaires extends JFrame {
     private JList<String> listeAffaires;
     private DefaultListModel<String> listeModel;
     private JTextArea detailsAffaire;
-    private JButton btnAjouter, btnModifier, btnSupprimer, btnAssocierCriminel;
+    private JButton btnAjouter, btnModifier, btnSupprimer, btnAssocierCriminel, btnGraphe;
     private Modele modele;
 
 
@@ -29,6 +29,8 @@ public class VueAffaires extends JFrame {
         // Haut
         JPanel panelHaut = new JPanel();
         JLabel label = new JLabel("Base des affaires criminelles");
+        btnGraphe = new JButton("Graphe");
+        panelHaut.add(btnGraphe, BorderLayout.WEST);
         panelHaut.add(label);
         add(panelHaut, BorderLayout.NORTH);
 
@@ -72,8 +74,19 @@ public class VueAffaires extends JFrame {
         btnAssocierCriminel.addActionListener(e -> associerCriminel());
         btnAjouterPlusieurs.addActionListener(e -> associerCriminelsMultiples());
         btnDissocierCriminel.addActionListener(e -> dissocierCriminel());
+        btnGraphe.addActionListener(e -> genererUnGraphe());
 
         setVisible(true);
+    }
+
+    private void genererUnGraphe(){
+        int index = listeAffaires.getSelectedIndex();
+        if (index >= 0) {
+            new VueGraphe(this.modele, index);
+        }
+        else{
+            JOptionPane.showMessageDialog(this, "Veuillez selectionner une affaire","Erreur",JOptionPane.ERROR_MESSAGE);
+        }
     }
 
     public VueAffaires(Modele modele, String lieu) {
